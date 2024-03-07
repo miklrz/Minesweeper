e4e964826871c7546803e6d28744f37b797eb870
miklrz <arz.mikhail@gmail.com>
2024-03-07 14:25:41 +0300

    1. Создано поле игры в scenebuilder 2. Подписываются нажатые кнопки 3. Создан массив

--- src/main/java/com/example/minesweeper/HelloController.java
+++ src/main/java/com/example/minesweeper/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.minesweeper;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    private char[][] gameField = new char[10][10];
+    @FXML
+    void checkMine(ActionEvent event) {
+        Button btn = (Button)event.getSource();
+        int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
+        int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
+        btn.setText("qwe");
+        System.out.printf("r: %d    c: %d\n",rowIndex,columnIndex);
+//      проверка на взрыв  gameField[columnIndex][rowIndex];
     }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
