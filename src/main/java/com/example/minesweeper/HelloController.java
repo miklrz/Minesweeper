509dcac8bc84eef25ed3e290ddb836d3b3561b07
miklrz <arz.mikhail@gmail.com>
2024-03-07 17:23:03 +0300

    Заполнение массива + подсчет мин

--- src/main/java/com/example/minesweeper/HelloController.java
+++ src/main/java/com/example/minesweeper/HelloController.java
@@ -10,21 +10,61 @@ import javafx.scene.layout.GridPane;
 
 public class HelloController {
 
+    private boolean gameStarted = false;
+
+    static char[][] getMinesArr(int N, int mines){
+        char[][] arr = new char[N][N];
+        int count = 0;
+        while(count < mines){
+            int newPath = ((int) (Math.random() * (N-1)*(N-1)));
+            if(arr[newPath%(N-1)][newPath/(N-1)] != 'x') {
+                arr[newPath%(N-1)][newPath/(N-1)] = 'x';
+                count ++;
+            }
+        }
+        return arr;
+    }
+
+//    void startGame(){
+//        char[][] arr = getMinesArr();
+//    }
+
+    static int countMinesAround(char[][] gameField, int rowIndex, int columnIndex){
+        int count = 0;
+        for(int x = columnIndex-1; x <= columnIndex+1; ++x){
+            for(int y = rowIndex-1; y <= rowIndex+1; ++y){
+                if(x == columnIndex && y == rowIndex) continue;
+                try{
+                    if(gameField[x][y] == 'x') count++;
+                }
+                catch(ArrayIndexOutOfBoundsException ignored){}
+            }
+        }
+        return count;
+    }
+
+    char[][] gameField = getMinesArr(10,30);
+
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
-    private char[][] gameField = new char[10][10];
+
     @FXML
     void checkMine(ActionEvent event) {
         Button btn = (Button)event.getSource();
         int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
-        btn.setText("qwe");
-        System.out.printf("r: %d    c: %d\n",rowIndex,columnIndex);
-//      проверка на взрыв  gameField[columnIndex][rowIndex];
+
+
+//        System.out.printf("r: %d    c: %d\n",rowIndex,columnIndex);
+        if(gameField[columnIndex][rowIndex] == 'x') btn.setText("пиздец!");
+        else{
+            btn.setText("Круто");
+        }
+
     }
 
     @FXML
