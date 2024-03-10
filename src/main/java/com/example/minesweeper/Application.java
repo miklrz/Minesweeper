207b0d842f7486cc27ac7e93917fe3238e2dac24
hxastur <arz.mikhail@gmail.com>
2024-03-10 20:47:47 +0300

    logo image setting ( не получилось )

--- src/main/java/com/example/minesweeper/Application.java
+++ src/main/java/com/example/minesweeper/Application.java
@@ -2,15 +2,27 @@ package com.example.minesweeper;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+//        try {
+//            Image img = new Image("src/res/icons/testLogo.jpg");
+//            stage.getIcons().add(img);
+//        }
+//        catch(Exception e) {
+//            System.out.println("Файл не найден! + " + e);
+//        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("game10x10-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Minesweeper");
         stage.setScene(scene);
         stage.show();
