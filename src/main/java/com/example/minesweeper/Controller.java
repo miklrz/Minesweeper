d4c8456f8ce8c0f1080c31167e6059f9274ee4c9
hxastur <arz.mikhail@gmail.com>
2024-03-13 22:00:46 +0300

    start mines generating func doing

--- src/main/java/com/example/minesweeper/Controller.java
+++ src/main/java/com/example/minesweeper/Controller.java
@@ -40,20 +40,44 @@ public class Controller {
     @FXML
     private URL location;
 
+    private boolean isStarted;
 
-    static char[][] getMinesArr(int N, int mines) {
+    static char[][] getMinesArr(int N, int mines, int x, int y) {
         char[][] arr = new char[N][N];
         int count = 0;
-        while (count < mines) {
-            int newPath = ((int) (Math.random() * (N - 1) * (N - 1)));
-            if (arr[newPath % (N - 1)][newPath / (N - 1)] != 'x') {
-                arr[newPath % (N - 1)][newPath / (N - 1)] = 'x';
-                count++;
+        //arr != ((y-1)*10 + x-1) to ((y+1)*10 + x+1)
+        // 10y +x -11 to 10y + x + 11
+        int border1 = 10*y +x -11;
+        int border2 = 10*y + x + 11;
+        if(border1 <0) border1 = 0;
+        if (border2 >= N*N) border2 = N*N-1;
+        while (count < mines) { // x !=
+            int chose = (int) (Math.random()*1);
+//            int newPath = ((int) (Math.random() * ((N-1)*(N-1))));
+//                if (arr[newPath % (N - 1)][newPath / (N - 1)] != 'x' && newPath <border1 && newPath > border2){
+//                    arr[newPath % (N - 1)][newPath / (N - 1)] = 'x';
+//                    count++;
+//                }
+            if(chose == 0){
+                int newPath = ((int) (Math.random() * border1));
+                if (arr[newPath % (N - 1)][newPath / (N - 1)] != 'x') {
+                    arr[newPath % (N - 1)][newPath / (N - 1)] = 'x';
+                    count++;
+                }
+                System.out.println(newPath % (N - 1)+ " " +newPath / (N - 1));
+
+            }
+            if(chose == 1){
+                int newPath = ((int) (Math.random() * (N*N - border2) + border2));
+                if (arr[newPath % (N - 1)][newPath / (N - 1)] != 'x') {
+                    arr[newPath % (N - 1)][newPath / (N - 1)] = 'x';
+                    count++;
+                }
+                System.out.println(newPath % (N - 1)+ " " +newPath / (N - 1));
             }
         }
         return arr;
     }
-
 
     int countMinesAround(char[][] gameField, int columnIndex, int rowIndex) {
         if (gameField[columnIndex][rowIndex] == 'x') {
@@ -98,9 +122,22 @@ public class Controller {
 
     @FXML
     void checkMine(ActionEvent event) {
+
         Button btn = (Button) event.getSource();
         int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
+
+        if(!isStarted){
+            gameField = getMinesArr(10,15,rowIndex,columnIndex);
+            isStarted = true;
+        }
+
+        for(int i = 0; i < 10; ++i){
+            for(int j = 0; j < 10; ++j){
+                System.out.print(gameField[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         String count = String.valueOf(countMinesAround(gameField, columnIndex, rowIndex));
         if(!count.equals("0")) btn.setText(count);
@@ -115,7 +152,6 @@ public class Controller {
 
     @FXML
     void initialize() {
-        gameField = getMinesArr(10, 20);
         buttons = new ArrayList<>(Arrays.asList(
                 button00, button10, button20, button30, button40, button50, button60, button70, button80, button90,
                 button01, button11, button21, button31, button41, button51, button61, button71, button81, button91,
@@ -129,12 +165,13 @@ public class Controller {
                 button09, button19, button29, button39, button49, button59, button69, button79, button89, button99
         ));
         endText.setText("");
-        for(int y = 0; y < 10; ++y){
-            for(int x = 0; x < 10; ++x){
-                System.out.print(gameField[x][y] + " ");
-            }
-            System.out.println();
-        }
+        isStarted = false;
+//        for(int y = 0; y < 10; ++y){
+//            for(int x = 0; x < 10; ++x){
+//                System.out.print(gameField[x][y] + " ");
+//            }
+//            System.out.println();
+//        }
     }
 
     @FXML
