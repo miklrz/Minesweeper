bb42e2ad326a1564f74da22fb113c2cf2bd519ea
hxastur <arz.mikhail@gmail.com>
2024-03-21 22:04:25 +0300

    qwe

--- src/main/java/com/example/minesweeper/Application.java
+++ src/main/java/com/example/minesweeper/Application.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -14,20 +15,10 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        try {
-            Image img = new Image("src/res/icons/testLogo.jpg");
-            stage.getIcons().add(img);
-        }
-        catch(Exception e) {
-            System.out.println("Файл не найден! + " + e);
-        }
+        FlowPane root = new FlowPane();
+//        root.getChildren().addAll("Hello",)
 
-        Group root = new Group();
-        Scene scene = new Scene(root,600,600, Color.ALICEBLUE);
 
-        stage.setTitle("Minesweeper");
-        stage.setScene(scene);
-        stage.show();
     }
 
     public static void main(String[] args) {
